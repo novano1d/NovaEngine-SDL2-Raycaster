e950f65e32e1af3538814e63d370856271087f89
novano1d <mmoor179@kent.edu>
2023-06-29 16:06:40 -0400

    fixes

--- sdlgame.hpp
+++ sdlgame.hpp
@@ -7,8 +7,8 @@
 #include <unordered_set>
 #include <string>
 
-#define INTERNAL_RENDER_RES_HORIZ 640
-#define INTERNAL_RENDER_RES_VERT 360
+#define INTERNAL_RENDER_RES_HORIZ 512
+#define INTERNAL_RENDER_RES_VERT 288
 
 // #define INTERNAL_RENDER_RES_HORIZ 240
 // #define INTERNAL_RENDER_RES_VERT 240
